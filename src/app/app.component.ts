ccb22fe73fd120b4d330f95566a1193bbb06aeab
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-27 09:55:27 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fbc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'firebootcamp-crm';
+}

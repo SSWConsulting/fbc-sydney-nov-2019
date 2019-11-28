d536fdb1f90f399a45eafececdb1aeb8e28511bd
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-28 16:59:55 +1100

    e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('firebootcamp-crm app is running!');
+    expect(page.getTitleText()).toEqual('Welcome, Sydney 🐨, to FIREBOOTCAMP-CRM - DEV');
   });
 
   afterEach(async () => {

7a14e63da803323357bde3ed0d5834a73004dcbb
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-28 16:50:11 +1100

    added unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,153 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { of } from 'rxjs';
+import { CompanyService } from './company/company.service';
+import { DebugElement } from '@angular/core';
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { BrowserModule, By } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { reducers , metaReducers, AllEffects} from './state';
+import { EffectsModule } from '@ngrx/effects';
+import { APP_BASE_HREF } from '@angular/common';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
+
+describe(`test unint testing`, () => {
+  it('should add 1+1', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+
+describe(`Component: App Component`, () => {
+  it('should create app component', () => {
+    const component = new AppComponent(null);
+    expect(component.title).toEqual('firebootcamp-crm');
+  });
+});
+
+
+describe(`Component: App Component`, () => {
+  let component;
+  let companyService;
+
+  beforeEach(() => {
+    companyService = {
+      getCompanies: () => of([{
+        name: 'My Fake Corp',
+        email: 'fake@news.com',
+        phone: 12345
+      }])
+    };
+    component = new AppComponent(companyService);
+  });
+
+  it('should have 1 fake company', () => {
+    component.ngOnInit();
+    component.companyCount$.subscribe(c => {
+      expect(c).toEqual(1);
+    });
+  });
+});
+
+
+describe(`Component: App Component`, () => {
+  let component;
+  let companyService;
+
+  beforeEach(() => {
+    companyService = {
+      getCompanies: () => {}
+    };
+    component = new AppComponent(companyService);
+  });
+
+  it('shoule use spyon', () => {
+    spyOn(companyService, 'getCompanies').and.returnValue(of([
+      {
+        name: 'My Fake Corp',
+        email: 'fake@news.com',
+        phone: 12345
+      },
+      {
+        name: 'My Fake Corp2',
+        email: 'fake2@news.com',
+        phone: 12222345
+      }
+    ]));
+    component.ngOnInit();
+    component.companyCount$.subscribe(c => {
+      expect(c).toEqual(2);
+    });
+  });
+});
+
+
+describe(`Testbed`, () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let companySvc: CompanyService;
+  let de: DebugElement;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CompanyListComponent,
+        CompanyTableComponent,
+        NotFoundComponent,
+        CompanyEditComponent
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgbModule,
+        HttpClientModule,
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+        StoreModule.forRoot(reducers, {
+          metaReducers,
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true
+          }
+        }),
+        EffectsModule.forRoot(AllEffects),
+      ],
+      providers: [ {provide: APP_BASE_HREF, useValue: '/'} ],
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    companySvc = TestBed.get(CompanyService);
   });
 
-  it(`should have as title 'firebootcamp-crm'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebootcamp-crm');
-  });
+  it(`company count = 1`, () => {
+    spyOn(companySvc, 'getCompanies').and.returnValue(of([
+      {
+        id: '12',
+        name: 'Fake Company C',
+        email: 'fakeEmail@ssw.com.au',
+        phone: 12345
+      }
+    ]));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('firebootcamp-crm app is running!');
+
+    expect(component.companyCount$.subscribe(c => {
+      expect(c).toEqual(1);
+    }));
+
+    let el  = fixture.debugElement.query(By.css('#company-count')).nativeElement;
+    expect(el.textContent).toEqual('1');
+
   });
 });
